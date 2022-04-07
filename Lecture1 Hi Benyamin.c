c5f9a82b2b4575bd787725e313a24765fca2f3ce
Byavuz07 <91992194+Byavuz07@users.noreply.github.com>
2022-04-07 12:15:12 +0300

    Lecture1 Hi Benyamin

---
+++ Lecture1 Hi Benyamin.c
@@ -0,0 +1,22 @@
+#include <stdio.h>     //Requested libraries inside the program
+#include <stdlib.h>
+
+//Written by Bunyamin Yavuz in 2022
+
+int main()
+{
+    //First Lecture (Comment line)
+    /*
+        First lecture multiple comment lines
+    */
+
+    //Print command (Print formatted)
+    //To tell the compiler that program ends ";"
+    //New line "\n"
+    printf("Hi Benyamin\n");
+
+
+
+    //Default value
+    return 0;
+}
