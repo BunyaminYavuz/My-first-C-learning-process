fecd31063e12db1b05ae70c370844cf616534a94
Byavuz07 <91992194+Byavuz07@users.noreply.github.com>
2022-04-07 12:17:14 +0300

    Lecture2 Variables

---
+++ Lecture2 Variables.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Written by Bunyamin Yavuz in 2022
+
+//Variables
+
+int main()
+{   /* -----Variable definition-----
+
+       Variabletype variableName = (value);
+       (int,char,double,float,long int,short int,boolean...)
+
+    */
+
+    //Integer
+    int number = 8;
+
+    //character
+    char letter = "b";
+
+    //Float
+    float fnumber = 8.5;
+            *
+            *
+            *
+
+    //Char array
+    char name[8] = "Benyamin";
+    char surname[5] = "yavuz";
+
+    //Printing char array as a string
+    //"%s" formatting method
+    printf("%s\n",name);
+    printf("%s",surname);
+
+    return 0;
+}
